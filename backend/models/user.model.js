699c63f209fcd764ecc3386881e275df513c0c7f
Syed Muhammed Bilal <intergo878@gmail.com>
2020-01-27 00:15:56 +0500

    Add files via upload

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minlength: 3
-    },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
 }, {
-    timestamp: true,
+  timestamps: true,
 });
 
 const User = mongoose.model('User', userSchema);
--- backend/server.js
+++ backend/server.js
@@ -8,17 +8,18 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log('MongoDB database connection established successfully');
+  console.log("MongoDB database connection established successfully");
 })
 
 const exercisesRouter = require('./routes/exercises');
-const usersRouter     = require('./routes/users');
+const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
