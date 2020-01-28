ae2632c60691204059f3075a995724657b5b0f0d
Syed Muhammed Bilal <intergo878@gmail.com>
2020-01-28 19:42:52 +0500

    Add files via upload

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
+import axios from 'axios';
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateExercises extends Component {
@@ -22,9 +23,17 @@ export default class CreateExercises extends Component {
     }
 
     componentDidMount() {
-        this.setState({
-            users: ['test user'],
-            username: 'test user'
+        axios.get('http://localhost:5000/users/')
+        .then(response => {
+          if (response.data.length > 0) {
+            this.setState({
+              users: response.data.map(user => user.username),
+              username: response.data[0].username
+            })
+          }
+        })
+        .catch((error) => {
+          console.log(error);
         })
     }
 
@@ -63,6 +72,9 @@ export default class CreateExercises extends Component {
         }
 
         console.log(exercise);
+
+        axios.post('http://localhost:5000/exercises/add', exercise)
+            .then(res => console.log(res.data));
 
         window.location = '/';
     }
